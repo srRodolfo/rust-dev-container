487c71aba829c2e4de72c802cc9a58c5c074b25c
srRodolfo <rodolfo@compilar.com.br>
2026-09-01 20:53:33 -0300

    ajuste no projeto

---
+++ projects/hello-world/target/debug/deps/hello_world-797fd1c433c806c9.d
@@ -0,0 +1,5 @@
+/projects/hello-world/target/debug/deps/hello_world-797fd1c433c806c9.d: src/main.rs
+
+/projects/hello-world/target/debug/deps/hello_world-797fd1c433c806c9: src/main.rs
+
+src/main.rs:
---
+++ projects/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/projects/hello-world/target/debug/hello-world: /projects/hello-world/src/main.rs
